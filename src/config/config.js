75d94c7e0b8388c9460de8bb8e01cf4d54002e7f
Memesneverchanges <pinki_1198@mail.ru>
2024-03-31 23:03:08 +0700

    docker deploying

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+let config = {
+    logger: {
+        appenders: { main: { type: "dateFile", filename: "./logs/log.log", encoding: 'UTF-8', pattern: "yyyy-MM-dd-hh", compress: true, numBackups: 24 * 365 } },
+        categories: { default: { appenders: ["main"], level: "all" } },
+    }
+}
+
+module.exports=config
